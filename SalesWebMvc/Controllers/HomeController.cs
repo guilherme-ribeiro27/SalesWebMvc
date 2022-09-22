bbe1071cc6e7ef6d769d0ee248e4db117ba67de5
Guilherme Ribeiro <guilherme.ribeiro062002@gmail.com>
2022-09-22 17:24:32 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models.ViewModels;
+
 namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
